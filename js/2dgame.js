b0dea23a6994aa9fbd184c1a1a32a962b0e244f0
Michael Specht <micha.specht@gmail.com>
2015-02-24 00:07:33 +0100

    yay

--- js/2dgame.js
+++ js/2dgame.js
@@ -1225,7 +1225,7 @@ function init_game(width, height, supersampling, data)
     if (!!('ontouchstart' in window))
     {
         var control = null;
-        control = $('<div>').addClass('control').html("<i class='fa fa-arrow-circle-left'></i>").css('right', '200px').css('bottom', '0');
+        control = $('<div>').addClass('control').html("<i class='fa fa-arrow-circle-left'></i>").css('right', '50px').css('bottom', '0');
         control.mousedown(function() {
             vars.pressed_keys[37] = true;
         });
